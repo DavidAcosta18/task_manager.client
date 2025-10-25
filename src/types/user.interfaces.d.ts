f0b03b9d16cff3f99d801a1010bccf8f281a8246
David <joose3mil@gmail.com>
2025-10-25 15:31:15 -0400

    feat:(users) modify user structure

--- src/types/user.interfaces.d.ts
+++ src/types/user.interfaces.d.ts
@@ -1,16 +1,9 @@
-import type { Language } from './language.enum';
 import type { RolesEnum } from './roles.enum';
 
 export interface IUser {
   _id: string;
   email: string;
-  phone: string;
-  profile: {
-    firstName: string;
-    lastName: string;
-    address: object;
-    avatarUrl: string;
-  };
-  language: Language;
+  firstName: string;
+  lastName: string;
   role: RolesEnum;
 }
